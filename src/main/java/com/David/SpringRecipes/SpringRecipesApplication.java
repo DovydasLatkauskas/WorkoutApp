3c0ee81181c9cd5acb78d2d1afa146271c6ac9d7
hardikchoraria <chorariahardik@gmail.com>
2023-04-01 14:09:40 +0100

    kggj

--- src/main/java/com/David/SpringRecipes/SpringRecipesApplication.java
+++ src/main/java/com/David/SpringRecipes/SpringRecipesApplication.java
@@ -1,13 +1,35 @@
 package com.David.SpringRecipes;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+
 
 @SpringBootApplication
 public class SpringRecipesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRecipesApplication.class, args); // no args taken
+	public static void main(String[] args) throws IOException, InterruptedException {
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://exercisedb.p.rapidapi.com/exercises/target/abs"))
+				.header("X-RapidAPI-Key", "409b178621mshc057f3f20ab8eafp15bac6jsnc3bfc1a5a71e")
+				.header("X-RapidAPI-Host", "exercisedb.p.rapidapi.com")
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
+
+
+
+		//SpringApplication.run(SpringRecipesApplication.class, args); // no args taken
 	}
 
 }
---
+++ src/main/java/com/David/SpringRecipes/models/Excercise.java
@@ -0,0 +1,42 @@
+package com.David.SpringRecipes.models;
+
+public class Excercise {
+    private String name;
+    private String targetMuscles;
+    private String equipment;
+    private String gifUrl;
+
+    public void Exercise(String name, String targetMuscles, String equipment, String gifUrl) {
+        this.name = name;
+        this.targetMuscles = targetMuscles;
+        this.equipment = equipment;
+        this.gifUrl = gifUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTargetMuscles() {
+        return targetMuscles;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public String getGifUrl() {
+        return gifUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" +
+                "name='" + name + '\'' +
+                ", targetMuscles='" + targetMuscles + '\'' +
+                ", equipment='" + equipment + '\'' +
+                ", gifUrl='" + gifUrl + '\'' +
+                '}';
+    }
+
+}
