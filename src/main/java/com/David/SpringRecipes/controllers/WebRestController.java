3a1600b7b690d3f0a203134391e20c4edea5bfaa
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-04-01 18:54:22 +0100

    cleaned up old files

--- src/main/java/com/David/SpringRecipes/controllers/WebRestController.java
+++ src/main/java/com/David/SpringRecipes/controllers/WebRestController.java
@@ -1,26 +1,10 @@
 package com.David.SpringRecipes.controllers;
 
 import com.David.SpringRecipes.models.Exercise;
-import com.David.SpringRecipes.models.Recipe;
-import com.David.SpringRecipes.services.RecipeService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
 
 @RestController
 public class WebRestController {
-    private final RecipeService recipeService;
-
-    public WebRestController(@Autowired RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
-
-    @GetMapping("/all-recipes")
-    public Collection<Recipe> get() {
-        return recipeService.getAll();
-    }
 
 //    @DeleteMapping("/delete/{name}")
 //    public void delete(@PathVariable String name){
@@ -29,11 +13,6 @@ public class WebRestController {
 //            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 //        }
 //    }
-    @PostMapping(value = "/upload")
-    public Recipe create(@RequestBody @Valid Recipe recipe){
-        recipeService.create(recipe);
-        return recipe;
-    }
 
     @PostMapping(value = "/add-exercise")
     public Exercise addExercise(@RequestBody Exercise exercise){
--- src/main/java/com/David/SpringRecipes/controllers/WebThymeleafController.java
+++ src/main/java/com/David/SpringRecipes/controllers/WebThymeleafController.java
@@ -1,56 +1,27 @@
 package com.David.SpringRecipes.controllers;
 
-import com.David.SpringRecipes.models.Exercise;
-import com.David.SpringRecipes.models.Recipe;
 import com.David.SpringRecipes.repositories.MuscleGroups;
 import com.David.SpringRecipes.services.ExerciseService;
-import com.David.SpringRecipes.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.*;
 
 @Controller
 public class WebThymeleafController {
-    private final RecipeService recipeService;
     private final ExerciseService exerciseService;
 
-    public WebThymeleafController(@Autowired RecipeService recipeService, ExerciseService exerciseService) {
-        this.recipeService = recipeService;
+    public WebThymeleafController(@Autowired  ExerciseService exerciseService) {
         this.exerciseService = exerciseService;
     }
-
-    @GetMapping("/{name}")
-    public String getByName(@PathVariable String name, Model model) {
-        Recipe recipe = recipeService.get(name);
-        if(recipe == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-        model.addAttribute("recipe", recipe);
-        return "recipe";
-    }
-    @GetMapping("/")
-    public String allRecipes(Model model) {
-        Collection<Recipe> recipes = recipeService.getAll();
-        model.addAttribute("recipes", recipes);
-        return "recipes-list";
-    }
-    @GetMapping("/upload")
-    public String uploadRecipe(Model model){
-        return "upload";
-    }
-    @GetMapping("/{name}/delete")
-    public String deleteRecipe(@PathVariable("name")String name){
-        Recipe recipe = recipeService.remove(name);
-        if(recipe == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-        return "redirect:/";
-    }
+//    @GetMapping("/{name}/delete")
+//    public String deleteRecipe(@PathVariable("name")String name){
+//        Recipe recipe = recipeService.remove(name);
+//        if(recipe == null){
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//        }
+//        return "redirect:/";
+//    }
     @GetMapping("/select-muscles")
     public String selectMuscles(Model model){
         model.addAttribute("muscleGroups", MuscleGroups.getMuscleGroups());
