891d86ec5883a54d7852010615daa1d197c37cbe
Aman Sharma <mail.aman@protonmail.com>
2023-04-01 13:30:34 +0100

    Added JS interactivity

--- js/mainHome.js
+++ js/mainHome.js
@@ -7,9 +7,27 @@ let workOuts = document.getElementsByClassName("workOuts");
 for( let workout of workOuts ){
   workout.addEventListener("mousemove", function(){
     workout.style.opacity = "100%"
+    workout.style.height = "14%"
+    workout.style.width = "92%"
+    workout.style.left = "1%"
+    //cursorStyling
+    cursorDiv.style.height = "60px";
+    cursorDiv.style.width = "60px";
+    cursorDiv.style.backgroundColor= "rgba(255, 255, 255, 1)";
+    cursorDiv.style.mixBlendMode = "difference";
+    cursorDiv.style.border= "none";
   })
   workout.addEventListener("mouseout", function(){
     workout.style.opacity = "20%"
+    workout.style.height = "12%"
+    workout.style.width = "90%"
+    workout.style.left = "2%"
+    //cursorStyling
+    cursorDiv.style.height = "40px";
+    cursorDiv.style.width = "40px";
+    cursorDiv.style.backgroundColor= "transparent";
+    cursorDiv.style.border= "2px solid white";
+    cursorDiv.style.mixBlendMode = "difference";
   })
 }
 
